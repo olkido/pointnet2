9f3e9f5c9aef0d6f8350e6d95dd297b8ab91b447
Olga Diamanti <olga.diamanti@autodesk.com>
2018-07-12 08:46:12 -0700

    variable feature size in pointnet++

--- models/pointnet2_part_seg.py
+++ models/pointnet2_part_seg.py
@@ -8,13 +8,13 @@ import numpy as np
 import tf_util
 from pointnet_util import pointnet_sa_module, pointnet_fp_module
 
-def placeholder_inputs(batch_size, num_point):
-    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 6))
+def placeholder_inputs(batch_size, num_point, dimension):
+    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, dimension))
     labels_pl = tf.placeholder(tf.int32, shape=(batch_size, num_point))
     return pointclouds_pl, labels_pl
 
 
-def get_model(point_cloud, is_training, bn_decay=None):
+def get_model_0 (point_cloud, is_training, bn_decay=None):
     """ Part segmentation PointNet, input is BxNx6 (XYZ NormalX NormalY NormalZ), output Bx50 """
     batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
@@ -40,6 +40,32 @@ def get_model(point_cloud, is_training, bn_decay=None):
 
     return net, end_points
 
+def get_model(point_cloud, is_training, bn_decay=None):
+    """ Part segmentation PointNet, input is BxNxD (XYZ Feature), output Bx50 """
+    batch_size = point_cloud.get_shape()[0].value
+    num_point = point_cloud.get_shape()[1].value
+    dim_point = point_cloud.get_shape()[2].value
+    end_points = {}
+    l0_xyz = tf.slice(point_cloud, [0,0,0], [-1,-1,3])
+    l0_points = tf.slice(point_cloud, [0,0,3], [-1,-1,dim_point-3])
+
+    # Set Abstraction layers
+    l1_xyz, l1_points, l1_indices = pointnet_sa_module(l0_xyz, l0_points, npoint=512, radius=0.2, nsample=64, mlp=[64,64,128], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer1')
+    l2_xyz, l2_points, l2_indices = pointnet_sa_module(l1_xyz, l1_points, npoint=128, radius=0.4, nsample=64, mlp=[128,128,256], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer2')
+    l3_xyz, l3_points, l3_indices = pointnet_sa_module(l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3')
+
+    # Feature Propagation layers
+    l2_points = pointnet_fp_module(l2_xyz, l3_xyz, l2_points, l3_points, [256,256], is_training, bn_decay, scope='fa_layer1')
+    l1_points = pointnet_fp_module(l1_xyz, l2_xyz, l1_points, l2_points, [256,128], is_training, bn_decay, scope='fa_layer2')
+    l0_points = pointnet_fp_module(l0_xyz, l1_xyz, tf.concat([l0_xyz,l0_points],axis=-1), l1_points, [128,128,128], is_training, bn_decay, scope='fa_layer3')
+
+    # FC layers
+    net = tf_util.conv1d(l0_points, 128, 1, padding='VALID', bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
+    end_points['feats'] = net 
+    net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp1')
+    net = tf_util.conv1d(net, 50, 1, padding='VALID', activation_fn=None, scope='fc2')
+
+    return net, end_points
 
 def get_loss(pred, label):
     """ pred: BxNxC,
